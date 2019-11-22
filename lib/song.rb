30a5ec8c8ab9d3370b13b161ad4c460dd74a8f2b
Stacy Johns <stacykjohns@icloud.com>
2019-11-22 03:52:45 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -55,7 +55,8 @@ class Song
   end
 
   def self.create_from_filename(file)
-    self.new_from_filename(file).tap{ |song| song.save }
+    song = self.new_from_filename(file)
+    song.save
   end
 
 end
