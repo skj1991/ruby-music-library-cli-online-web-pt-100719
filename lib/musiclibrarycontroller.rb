4da7c9d8a710d8212ffbcc2faedd945c7d4a57cd
Stacy Johns <stacykjohns@icloud.com>
2019-11-22 05:22:33 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -17,7 +17,20 @@ class MusicLibraryController
       puts "What would you like to do?"
       input = gets until input == "exit"
 
-      
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
 
 
   end
